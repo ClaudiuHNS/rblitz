fda97588218c6484148c1397aa2bd6484ac308f0
Lukas Wirth <lukastw97@gmail.com>
2019-01-12 14:15:10 +0100

    fix formatting in enet-sys

--- enet-sys/src/lib.rs
+++ enet-sys/src/lib.rs
@@ -1,4 +1,10 @@
-#![allow(clippy::identity_op, clippy::unreadable_literal, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![allow(
+    clippy::identity_op,
+    clippy::unreadable_literal,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
 use core::option::Option;
 use std::os::raw::*;
 
